58ad5557f0d7497350a7def2005f2fc071160c92
Vlad Mocanu <vladmoca90@gmail.com>
2024-01-11 21:37:34 +0000

    Added the keys back again

--- airport-project/src/next.config.js
+++ airport-project/src/next.config.js
@@ -1,4 +1,4 @@
-/** @type {import('next').NextConfig} */
+/* @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
     async headers() {
@@ -7,10 +7,11 @@ const nextConfig = {
                 // matching all API routes
                 source: "app/api/**/",
                 headers: [
-                    {
-                        key: "Cache-Control",
-                        value: "s-maxage=1, stale-while-revalidate=59",
-                    },
+                    { key: "Cache-Control", value: "s-maxage=1, stale-while-revalidate=59"},
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
                 ]
             }
         ]
