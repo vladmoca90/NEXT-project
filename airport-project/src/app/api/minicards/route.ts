167c1071a0cfdc32ba62f1df511bb744b142effd
Vlad Mocanu <vladmoca90@gmail.com>
2024-01-11 21:55:55 +0000

    Fixing all

--- airport-project/src/app/api/minicards/route.ts
+++ airport-project/src/app/api/minicards/route.ts
@@ -12,11 +12,6 @@ export function GET(request: NextRequest) {
         },
         {
             status: 200,
-            headers: {
-                "Cache-Control": "public, s-maxage=1",
-                "CDN-Cache-Control": "public, s-maxage=60",
-                "Vercel-CDN-Cache-Control": "public, s-maxage=3600",
-            },
         },
     );
 }
