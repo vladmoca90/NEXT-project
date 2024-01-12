f2b5633e71b2e8ed2ef5445acb6d096d7a087cc4
Vlad Mocanu <vladmoca90@gmail.com>
2024-01-12 03:32:53 +0000

    Reversed

--- airport-project/src/app/api/minicards/route.ts
+++ airport-project/src/app/api/minicards/route.ts
@@ -12,13 +12,6 @@ export async function GET(request: NextRequest) {
         },
         {
             status: 200,
-            headers: {
-                "Access-Control-Allow-Private-Network": "true",
-                "Access-Control-Allow-Credentials": "true",
-                "Access-Control-Allow-Origin": "https://airport-next-new.vercel.app",
-                "Access-Control-Allow-Methods": "GET,DELETE,PATCH,POST,PUT",
-                "Access-Control-Allow-Headers": "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-            }
         },
     );
 }
