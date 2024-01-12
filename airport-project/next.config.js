1e366d1e3b817246f8549085a3fcd99ea116f87e
Vlad Mocanu <vladmoca90@gmail.com>
2024-01-12 03:14:13 +0000

    Reversed the pathnames

--- airport-project/next.config.js
+++ airport-project/next.config.js
@@ -4,7 +4,7 @@ module.exports = {
         return [
             {
                 // matching all API routes
-                source: "src/app/api/minicards/route.ts",
+                source: "/api/:path*",
                 headers: [
                     { key: "Access-Control-Allow-Private-Network", value: "true" },
                     { key: "Access-Control-Allow-Credentials", value: "true" },
