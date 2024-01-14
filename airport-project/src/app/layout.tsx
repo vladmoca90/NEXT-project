2fac7928c4bdf204fb0e2751075b911de9897f81
Vlad Mocanu <vladmoca90@gmail.com>
2024-01-14 20:43:24 +0000

    Left the out.scss file only

--- airport-project/src/app/layout.tsx
+++ airport-project/src/app/layout.tsx
@@ -3,23 +3,23 @@ import { Inter } from "next/font/google";
 import AirportNav from "./Navigation";
 import AirportFooter from "./Footer";
 
-// styles
-import "./styles/airport-guide.css";
-import "./styles/booking-details.css";
-import "./styles/booking-popup.css";
-import "./styles/carousel.css";
-import "./styles/flight-information.css";
-import "./styles/flights.css";
-import "./styles/fonts.css";
-import "./styles/footer.css";
-import "./styles/global.css";
-import "./styles/help.css";
-import "./styles/homepage.css";
-import "./styles/maps.css";
-import "./styles/navigation.css";
-import "./styles/prepare.css";
-import "./styles/search-bar.css";
-import "./styles/shop-and-eat.css";
+// all .css styles
+// import "./styles/airport-guide.css";
+// import "./styles/booking-details.css";
+// import "./styles/booking-popup.css";
+// import "./styles/carousel.css";
+// import "./styles/flight-information.css";
+// import "./styles/flights.css";
+// import "./styles/fonts.css";
+// import "./styles/footer.css";
+// import "./styles/global.css";
+// import "./styles/help.css";
+// import "./styles/homepage.css";
+// import "./styles/maps.css";
+// import "./styles/navigation.css";
+// import "./styles/prepare.css";
+// import "./styles/search-bar.css";
+// import "./styles/shop-and-eat.css";
 
 //using tailwind
 import "./styles/output.css";
