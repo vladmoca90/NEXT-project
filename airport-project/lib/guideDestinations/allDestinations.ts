85a6d315584655e40626136babae6657b0565829
Vlad Mocanu <vladmoca90@gmail.com>
2024-01-19 05:21:45 +0000

    Added "The most popular destination!" again

--- airport-project/lib/guideDestinations/allDestinations.ts
+++ airport-project/lib/guideDestinations/allDestinations.ts
@@ -3,7 +3,7 @@ import { Destination } from "./destination";
 export const allDestinations: Destination[] = [
     {
         description: "Dubai, UAE",
-        title: "The most popular destination of all!",
+        title: "The most popular destination!",
         url: "https://www.britishairways.com/en-gb/destinations/dubai/flights-to-dubai",
         urlText: "Visit Dubai",
     },
