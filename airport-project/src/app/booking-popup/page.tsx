0ba441cd113a71d24dfc4fcb5a690d9149405c3b
Vlad Mocanu <vladmoca90@gmail.com>
2024-01-21 21:41:09 +0000

    Testing the new component #3

--- airport-project/src/app/booking-popup/page.tsx
+++ airport-project/src/app/booking-popup/page.tsx
@@ -62,7 +62,7 @@ export default function BookingPopUp() {
             <div className="booking-bottom">
                 <div id="bookingBtn" className="booking-btn-container">
                     <Link href={{
-                        pathname: "/booking-content",
+                        pathname: "/booking-details",
                         query: {
                             "surname": surnameText.trim(),
                             "bookingCode": bookingText.trim(),
