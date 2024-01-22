370cf4c36ad3d54c356a40d0271bda68b19c352e
Vlad Mocanu <vladmoca90@gmail.com>
2024-01-22 00:06:26 +0000

    Testing the lazy loading icon

--- airport-project/src/app/booking-details/page.tsx
+++ airport-project/src/app/booking-details/page.tsx
@@ -6,7 +6,7 @@ import loadingImg from "../../../public/images/loading.gif";
 
 const LazyLoading = dynamic(() => import("../booking-content/page"), {
     ssr: false,
-        loading: () => <Image width={100} height={100} alt="Loading icon" className="loading-img" src={loadingImg} />
+    loading: () => <Image width={100} height={100} alt="Loading icon" className="loading-img" src={loadingImg} />
 });
 
 export default function BookingDetails({ searchParams }: {
