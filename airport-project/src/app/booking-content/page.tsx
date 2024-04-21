c8f4bb24fee8331bc2126806b563aca1de3dc718
Vlad Mocanu <vladmoca90@gmail.com>
2024-04-21 01:04:41 +0100

    Renamed the id as #returnBtn

--- airport-project/src/app/booking-content/page.tsx
+++ airport-project/src/app/booking-content/page.tsx
@@ -50,7 +50,7 @@ export default function BookingContent({ searchParams }: {
         return (
             <div className="booking-details--not-found">
                 <h3>No booking could be found!</h3>
-                <div id="backBtn">
+                <div id="returnBtn">
                     <a className="return-btn" href="/">{arrowLeft} Return to Homepage</a>
                 </div>
             </div>
